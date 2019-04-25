792e4be31537d4bef670f522d00d95d8f4ffc6d5
Tom Hanstra <hanstra@nd.edu>
2019-04-25 16:44:03 -0400

    Create linking script

---
+++ bin/home_links.sh
@@ -0,0 +1,15 @@
+ln -s ~/hanstra_local/.aliasb  ~/.aliasb
+ln -s ~/hanstra_local/..bash_functions  ~/..bash_functions
+ln -s ~/hanstra_local/.bash_profile  ~/.bash_profile
+ln -s ~/hanstra_local/.bashrc  ~/.bashrc
+ln -s ~/hanstra_local/.make_common_b  ~/.make_common_b
+ln -s ~/hanstra_local/.make_oracle_sid_b  ~/.make_oracle_sid_b
+ln -s ~/hanstra_local/.make_path_b  ~/.make_path_b
+ln -s ~/hanstra_local/.set_prompt_b  ~/.set_prompt_b
+mkdir ~/bin
+ln -s ~/hanstra_local/bin/psgrep ~/bin/psgrep
+ln -s ~/hanstra_local/bin/set_prompt ~/bin/set_prompt
+ln -s ~/hanstra_local/bin/tarcp ~/bin/tarcp
+ln -s ~/hanstra_local/bin/taws ~/bin/taws
+ln -s ~/hanstra_local/bin/taws_oregon ~/bin/taws_oregon
+ln -s ~/hanstra_local/bin/taws_test ~/bin/taws_test
