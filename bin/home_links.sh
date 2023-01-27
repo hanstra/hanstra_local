8295ad92758382e5119b02d2074cb4bda7e931d3
Tom Hanstra <hanstra@nd.edu>
2023-01-27 16:40:09 -0500

    Add find-tasks to the links

--- bin/home_links.sh
+++ bin/home_links.sh
@@ -7,6 +7,7 @@ ln -s ~/hanstra_local/roles/user_hanstra/files/home/.make_oracle_sid_b  ~/.make_
 ln -s ~/hanstra_local/roles/user_hanstra/files/home/.make_path_b  ~/.make_path_b
 ln -s ~/hanstra_local/roles/user_hanstra/files/home/.set_prompt_b  ~/.set_prompt_b
 mkdir ~/bin
+ln -s ~/hanstra_local/roles/user_hanstra/files/home/bin/find-task.sh ~/bin/find-task.sh
 ln -s ~/hanstra_local/roles/user_hanstra/files/home/bin/psgrep ~/bin/psgrep
 ln -s ~/hanstra_local/roles/user_hanstra/files/home/bin/set_prompt ~/bin/set_prompt
 ln -s ~/hanstra_local/roles/user_hanstra/files/home/bin/tarcp ~/bin/tarcp
